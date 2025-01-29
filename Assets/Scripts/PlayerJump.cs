8df96b987b72d1b031982c518edd4bec904c4dc8
DANIEL BARBOSA DE ARAUJO LIMA <43988711870@info.local>
2025-01-29 16:37:55 -0300

    feat: script pulo

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+//Requisitamos o componente Rigidbody
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerJump : MonoBehaviour
+{
+    //Variável que controla a força do pulo
+    public float jumpForce = 7.0f;
+
+    //Variável que controla a massa/peso
+    public float mass = 1.0f;
+
+    //Acessamos o componente Rigidbody através dessa variável
+    private Rigidbody rigidbody;
+
+
+    //Variável de controle que nos diz se o personagem está ou não no chão
+    private bool isGround = false;
+
+    void Start()
+    {
+        //Assim que o script  é executado, obtemos o componente Rigidbody e atribuimos a nossa variável
+        rigidbody = GetComponent<Rigidbody>();
+
+        //Definimos o valor da massa via script
+        rigidbody.mass = mass;
+    }
+
+
+    void Update()
+    {
+        //Verificamos se o usuário NÃO pressionou a tecla Space ou se não está no chão
+        if (!Input.GetKeyDown(KeyCode.Space) || !isGround)
+            return;
+
+        //Adicionamos uma força ao Rigidbody
+        rigidbody.AddForce(
+            Vector3.up * jumpForce, //Para fazer o personagem pular, então multiplicamos (0, 1, 0) pelo valor do pulo
+            ForceMode.Impulse // Ajustamos a força para o tipo Impulse
+            );
+    }
+
+    //Verifica se o personagem tocou no chão
+    void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+    }
+
+    //Verifica se o personagem saiu do chão
+    void OnCollisionExit(Collision collision)
+    {
+        isGround = false;
+    }
+}
+
